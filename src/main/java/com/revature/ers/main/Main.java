db2473c1b1f91b4c6133396fce4a12765970d238
Hieu Le <htl43@drexel.edu>
2020-12-21 23:17:43 -0500

    maven build

--- src/main/java/com/revature/ers/main/Main.java
+++ src/main/java/com/revature/ers/main/Main.java
@@ -10,16 +10,14 @@ public class Main {
 	private static UserService userService = new UserService();
 
 	public static void main(String[] args) {
-		ErsUserRole ersRole = new ErsUserRole(1, "");
-		ErsUser ersUser = new ErsUser("emp2", "emp2", "Maria", "Garcia", "maga@outlook.com", ersRole);
+		ErsUserRole ersRole = new ErsUserRole(2, "");
+		ErsUser ersUser = new ErsUser("mng2", "mng2", "Ben", "Franklin", "benfr@gmail.com", ersRole);
+		String encrPass = userService.encryptPassword(ersUser.getPassword());
+		ersUser.setPassword(encrPass);
 		
-		ErsUserRole mngR = new ErsUserRole(2, "");
-		ErsUser mng1 = new ErsUser("mng1", "mng1", "Hieu", "Le", "hile@gmail.com", mngR);
-		ErsUser mng2 = new ErsUser("mng2", "mng2", "Thomas", "Brown", "thobr@yahoo.com", mngR);
 		 try {
 			userService.create(ersUser);
-			userService.create(mng1);
-			userService.create(mng2);
+			
 		} catch (BusinessException e) {
 			e.printStackTrace();
 		}
