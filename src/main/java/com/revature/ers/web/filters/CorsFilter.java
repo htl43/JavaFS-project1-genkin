90d3cb0316ac39002c63334cea0a75ac8c29ab16
Hieu Le <htl43@drexel.edu>
2021-01-01 01:31:47 -0500

    setup testing cords 1

--- src/main/java/com/revature/ers/web/filters/CorsFilter.java
+++ src/main/java/com/revature/ers/web/filters/CorsFilter.java
@@ -37,7 +37,8 @@ public class CorsFilter implements Filter {
 		// Which is important, because we are going to set
 		// headers, which is specific to HTTP
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.setHeader("Access-Control-Allow-Origin", "Origin"); 
+		
+		res.setHeader("Access-Control-Allow-Origin", "null"); // Allow all origins
 		
 		res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 		// Allow specific HTTP Verbs
