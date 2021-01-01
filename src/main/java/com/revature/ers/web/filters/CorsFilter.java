918dcfd9bd654e4f9aa23cf62aba4c263351863a
Hieu Le <htl43@drexel.edu>
2021-01-01 14:00:44 -0500

    stupid aws

--- src/main/java/com/revature/ers/web/filters/CorsFilter.java
+++ src/main/java/com/revature/ers/web/filters/CorsFilter.java
@@ -43,7 +43,8 @@ public class CorsFilter implements Filter {
 		res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD");
 		// Allow specific HTTP Verbs
 		
-		res.setHeader("Access-Control-Allow-Headers", "*");
+		res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type,"
+				+ "Access-Control-Request-Method, Access-Control-Request-Headers");
 		// Allow specific HTTP Headers (there's a fair few)
 		
 		res.setHeader("Access-Control-Allow-Credentials", "true");
