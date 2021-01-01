5ad78faca7710e664c6ccaf69da581b448fb5ea1
Hieu Le <htl43@drexel.edu>
2020-12-31 22:07:21 -0500

    test jenkins webhook

--- src/main/java/com/revature/ers/utilities/DbUtilProps.java
+++ src/main/java/com/revature/ers/utilities/DbUtilProps.java
@@ -5,7 +5,7 @@ public class DbUtilProps {
 	//Local Database URL
 //	public static final String URL="jdbc:postgresql://localhost:5432/postgres";
 	
-	//AWS-RDS-URL
+
 	public static final String URL="jdbc:postgresql://database-1.ckkj5dk7yaq3.us-east-2.rds.amazonaws.com:5432/postgres";
 	public static final String DRIVER="org.postgresql.Driver";
 }
